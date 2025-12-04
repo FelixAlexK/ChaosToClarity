072f7a0ad6392dbbc0b45a664be4ffc6187d0ffa
Felix Kuhbier <felix.kuhbier@yahoo.com>
2025-12-05 00:21:42 +0100

    feat: add wasm plugin and configure server polling in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import wasm from 'vite-plugin-wasm'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    wasm(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
@@ -13,6 +15,16 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+
+  optimizeDeps: {
+    exclude: ['@synckit-js/sdk'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
