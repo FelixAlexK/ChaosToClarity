72330df593ea73b5c03521d989241f8f538ec2e7
Felix Kuhbier <felix.kuhbier@yahoo.com>
2025-12-05 00:21:23 +0100

    feat: wrap App component with ThemeProvider and add Toaster for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'sonner'
 import App from './App.tsx'
+import { ThemeProvider } from './components/themeProvider.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+
+      <App />
+      <Toaster position="top-center" />
+    </ThemeProvider>
   </StrictMode>,
 )
