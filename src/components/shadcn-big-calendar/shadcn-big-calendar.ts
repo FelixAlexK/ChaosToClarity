9633713b86def2f4190d7c0d6b861594e31a24a4
Felix Kuhbier <felix.kuhbier@yahoo.com>
2025-12-05 13:02:03 +0100

    style: simplify import statements and remove unnecessary semicolons in shadcn-big-calendar

--- src/components/shadcn-big-calendar/shadcn-big-calendar.ts
+++ src/components/shadcn-big-calendar/shadcn-big-calendar.ts
@@ -1,8 +1,8 @@
-import { Calendar } from "react-big-calendar";
+import { Calendar } from 'react-big-calendar'
 
-import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
-import "./shadcn-big-calendar.css";
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css'
+import './shadcn-big-calendar.css'
 
-const ShadcnBigCalendar = Calendar;
+const ShadcnBigCalendar = Calendar
 
-export default ShadcnBigCalendar;
+export default ShadcnBigCalendar
