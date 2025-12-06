164ada8513b757066ea33132cad132117332fa78
Felix Kuhbier <felix.kuhbier@yahoo.com>
2025-12-06 03:28:13 +0100

    feat: remove OutputBox component to simplify UI structure

--- src/components/OutputBox.tsx
+++
@@ -1,9 +0,0 @@
-export function OutputBox({ output }: { output: string }) {
-  return (
-    <div className="w-full max-w-4xl h-80 mx-auto border border-gray-300 rounded-lg p-4 bg-gray-50">
-      <p className="text-left">
-        {output}
-      </p>
-    </div>
-  )
-}
