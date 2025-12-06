69ad902200ae6904aef460a06f7e4a87b41ac891
Felix Kuhbier <felix.kuhbier@yahoo.com>
2025-12-06 15:45:08 +0100

    feat: add visualizer plugin to Vite configuration for improved build analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import wasm from 'vite-plugin-wasm'
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    visualizer({ open: true }) as PluginOption
   ],
 
   server: {
@@ -22,9 +24,37 @@ export default defineConfig({
     },
   },
 
+  build: {
+    rollupOptions: {
+      output: {
+        advancedChunks: {
+          groups: [
+            {
+              test: /node_modules\/react/,
+              name: 'react',
+            },
+            {
+              test: /node_modules\/@tailwindcss\/vite/,
+              name: 'tailwind',
+            },
+            {
+              test: /node_modules\/date-fns\/tz/,
+              name: 'date-fns',
+            },
+            {
+              test: /node_modules\/@google\/genai/,
+              name: 'genai',
+            }
+          ]
+        }
+      },
+    },
+  },
+
   optimizeDeps: {
     exclude: ['@synckit-js/sdk'],
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
